528748e4e75caba6bf3a24c57198e8f869edb9e4
jcnoga <164394666+jcnoga@users.noreply.github.com>
2026-06-10 09:50:13 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: "AIzaSyCk7j2q4ZSid99mTPp9yp12dKS2XM_gIVI",
-    authDomain: "sonhorealmix.firebaseapp.com",
-    projectId: "sonhorealmix",
-    storageBucket: "sonhorealmix.firebasestorage.app",
-    messagingSenderId: "133225034441",
-    appId: "1:133225034441:web:4f87f4807837cccb6f5318"
-  });
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCk7j2q4ZSid99mTPp9yp12dKS2XM_gIVI",
+  authDomain: "sonhorealmix.firebaseapp.com",
+  projectId: "sonhorealmix",
+  storageBucket: "sonhorealmix.firebasestorage.app",
+  messagingSenderId: "133225034441",
+  appId: "1:133225034441:web:4f87f4807837cccb6f5318",
+  measurementId: "G-HL8K48JZBS"
+};
 var db = firebase.firestore();
 var auth = firebase.auth();
 var storage = firebase.storage();
